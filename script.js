084dcdd4f95fd9682d8089f997584b97e8394877
Sardarovkenan <kenanserdarov20@gmail.com>
2022-12-20 00:29:06 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+let film = [
+    ['The Godfather' , 'Casablanka' , 'Gladiator' , 'Titanic' , 'Rocky' ] ,
+    ['Patton' , 'The Pianist' , 'Goodfellas' , 'The Green Mile' , 'Home Alone'],
+    ['Avatar' , 'Harry Potter' , 'Hobbit' ]
+]
+document.write(film[0][4][3]+film[0][1][3]+film[0][1][7]+film[0][1][3]+film[0][1][7])
+console.log(film[0][4][3]+film[0][1][3]+film[0][1][7]+film[0][1][3]+film[0][1][7]+ " "+film[0][1][2]+film[0][1][3]+film[2][0][5]+film[1][2][3]+film[0][1][3]+film[2][0][5]+film[0][4][1]+film[2][0][1])
